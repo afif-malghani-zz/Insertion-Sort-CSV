ed87a29bffb676945e17d3882dc8e44df110eab6
Muhammad Afif Ul Hasnain Malghani <50712084+afif-malghani@users.noreply.github.com>
2020-07-05 12:05:40 -0700

    Update insertion.py
    Added comments

--- insertion.py
+++ insertion.py
@@ -1,18 +1,30 @@
+# take list from csv read as input
 def Sort(list1, col):
+    
+#     itrate over rows
     for i in range(len(list1)):
+#         skip for row 1 because it has names of columns
         if i <=1:
             continue
+#         check if `col` column of current row is smaller than that of previous row
         elif list1[i][col] < list1[i-1][col]:
+#         store in temporary variable
             temp = list1[i]
+#     create variable j to itrate all previous rows
             j = i -1
             while j >= 1:
+#             if any row with column `col` value greater than temp
                 if list1[j][col]>temp[col]:
+#         move it to the next index, repeat till reach a value smaller than temp[col]
                     list1[j+1] = list1[j]
+#     if previous row is index 1, after moving it to next index put temp at index 1 
                     if j == 1:
                         list1[j] = temp
-                # elif list1[j+1] == temp:
+#                 if prev_row[col] <= temp
                 else:
+#         Place temp at next index
                     list1[j+1] = temp
+#     breat so that this process does not repeat for higher numbers
                     break
                 j -= 1
     return list1
